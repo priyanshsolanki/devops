d51d8949894d1e067136873be277dda1d74870c6
Priyansh Solanki <priyanshsolanki1@gmail.com>
2021-04-06 18:19:28 +0530

    Slight Modification

--- src/components/Pipelines/index.js
+++ src/components/Pipelines/index.js
@@ -15,7 +15,59 @@ const Pipelines = (props) => {
     setPipline(res.data);  
   });
   },[]);
-  
+  // const pipeline=useState([
+  //   {
+  //     titleLogo:"CI",
+  //     title:"CI Pipeline",
+  //     path:"/pipelines/cipipeline",
+  //     platforms:[
+  //       "GitLab",
+  //       "GitHub",
+  //       "Azure"
+  //     ]
+  //   },
+  //   {
+  //     titleLogo:"CD",
+  //     title:"CD Pipeline",
+  //     path:"/pipelines/cdpipeline",
+  //     platforms:[
+  //         "GitLab",
+  //         "Azure"]
+  //    },
+  //    {
+  //     titleLogo:"PR",
+  //     title:"PR Pipeline",
+  //     path:"/pipelines/prpipeline",
+  //     platforms:[
+  //         "GitLab",
+  //         "Azure"]
+  //    },
+  //    {
+  //     titleLogo:"AP",
+  //     title:"Artifact Promoting Pipeline",
+  //     path:"/pipelines/appipeline",
+  //       platforms:[
+  //         "GitLab",
+  //         "Azure"]
+  //    },
+  //    {
+  //     titleLogo:"AT",
+  //     title:"API Test Pipeline",
+  //     path:"/pipelines/atpipeline",
+  //       platforms:[
+  //         "GitLab",
+  //         "Azure"]
+  //    },
+  //    {
+  //     titleLogo:"LP",
+  //     title:"Load & Performance Test Pipeline",
+  //     path:"/pipelines/lppipeline",
+  //       platforms:[
+  //         "GitLab",
+  //         "Azure"]
+  //    }        
+  // ]);
+
 
   return(
     <div style={{margin:'20px'}}>
