038916219bd33dae0fcd7c1e8f9d9b2f2a2394a7
Priyansh Solanki <priyanshsolanki1@gmail.com>
2021-04-06 18:12:07 +0530

    Fake API added

--- src/components/Pipelines/index.js
+++ src/components/Pipelines/index.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import Axios from 'axios';
 import PiplineItem from './PipelineItem';
 import PlatformItem from './PipelineItem/PlatformItem';
 import './style.css';
@@ -8,59 +9,13 @@ import './style.css';
 **/
 
 const Pipelines = (props) => {
-  const [pipeline,setPipeLine]=useState([
-    {
-      titleLogo:"CI",
-      title:"CI Pipeline",
-      path:"/pipelines/cipipeline",
-      platforms:[
-        "GitLab",
-        "GitHub",
-        "Azure"
-      ]
-    },
-    {
-      titleLogo:"CD",
-      title:"CD Pipeline",
-      path:"/pipelines/cdpipeline",
-      platforms:[
-          "GitLab",
-          "Azure"]
-     },
-     {
-      titleLogo:"PR",
-      title:"PR Pipeline",
-      path:"/pipelines/prpipeline",
-      platforms:[
-          "GitLab",
-          "Azure"]
-     },
-     {
-      titleLogo:"AP",
-      title:"Artifact Promoting Pipeline",
-      path:"/pipelines/appipeline",
-        platforms:[
-          "GitLab",
-          "Azure"]
-     },
-     {
-      titleLogo:"AT",
-      title:"API Test Pipeline",
-      path:"/pipelines/atpipeline",
-        platforms:[
-          "GitLab",
-          "Azure"]
-     },
-     {
-      titleLogo:"LP",
-      title:"Load & Performance Test Pipeline",
-      path:"/pipelines/lppipeline",
-        platforms:[
-          "GitLab",
-          "Azure"]
-     }        
-  ]);
-
+  const [pipeline,setPipline]=useState([]);
+  useEffect(()=>{
+    Axios.get('https://api.mocki.io/v1/56500361').then(res=>{console.log(res.data)
+    setPipline(res.data);  
+  });
+  },[]);
+  
 
   return(
     <div style={{margin:'20px'}}>
